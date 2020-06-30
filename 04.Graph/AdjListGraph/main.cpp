f7a42b9db4770cae014db71fdda6aedc49639fb0
jihyuk124 <jihyuk124@naver.com>
2020-06-30 16:01:58 +0900

    그래프 구현(인접행렬, 인접 리스트)

---
+++ 04.Graph/AdjListGraph/main.cpp
@@ -0,0 +1,43 @@
+﻿#include<iostream>
+#include<list>
+using namespace std;
+
+class Graph
+{
+public:
+	Graph()
+		: size(0)
+		, vertices{0,}
+	{
+	}
+
+	void insertVertex(char vertex)
+	{
+		vertices[size++] = vertex;
+	}
+
+	void insertEdge(int u, int v)
+	{
+		setEdge(u, v, 1);
+		setEdge(v, u, 1);
+	}
+
+	void setEdge(int u, int v, int w)
+	{
+		adj[u].push_back(pair<int, int>(v, w));
+	}
+
+	bool isFull() { return size >= MAX_VTXS; }
+	bool isEmpty() { return size == 0; }
+
+private:
+	static const int MAX_VTXS = 256;
+	int size;
+	char vertices[MAX_VTXS];
+	list<pair<int,int>> adj[MAX_VTXS];
+};
+
+int main(void)
+{
+	Graph g;
+}
---
+++ 04.Graph/AdjMatGraph/main.cpp
@@ -0,0 +1,44 @@
+﻿#include<iostream>
+
+using namespace std;
+
+class Graph
+{
+public:
+	Graph()
+		: size(0)
+		, vertices{ 0, }
+		, adj{ {0,} }
+	{
+	}
+
+	void insertVertex(char vertex)
+	{
+		vertices[size++] = vertex;
+	}
+
+	void insertEdge(int u, int v)
+	{
+		setEdge(u, v, 1);
+		setEdge(v, u, 1);
+	}
+
+	void setEdge(int u, int v, int w)
+	{
+		adj[u][v] = w;
+	}
+
+	bool isFull() { return size >= MAX_VTXS; }
+	bool isEmpty() { return size == 0; }
+
+private:
+	static const int MAX_VTXS = 256;
+	int size;
+	char vertices[MAX_VTXS];
+	int adj[MAX_VTXS][MAX_VTXS];
+};
+
+int main(void)
+{
+	Graph g;
+}
