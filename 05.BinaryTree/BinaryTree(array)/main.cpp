7ccac71e3d6d843e8e3e25ef53df1489c1c78d12
DESKTOP-IVK43N8\jihyu <jihyuk124@naver.com>
2020-07-23 02:56:19 +0900

    이진 트리 구현

---
+++ 05.BinaryTree/BinaryTree(array)/main.cpp
@@ -0,0 +1,206 @@
+﻿#include <iostream>
+#include <queue>
+
+using namespace std;
+
+class BinaryTree
+{
+public:
+	BinaryTree();
+	~BinaryTree();
+
+	// tree treaversal
+	void preorder();
+	void inorder();
+	void postorder();
+	void levelorder();
+
+	int getCount();
+	int getHeight();
+
+	void insert_node(int idx, char value);
+
+private:
+	void preorder(int idx);
+	void inorder(int idx);
+	void postorder(int idx);
+
+	int getCount(int idx);
+	int getHeight(int idx);
+
+private:
+	static const int MAX = 100;
+
+	char values[MAX];
+	int size;
+};
+
+
+BinaryTree::BinaryTree() : values{ 0 }
+{
+}
+
+BinaryTree::~BinaryTree()
+{
+}
+
+void BinaryTree::preorder()
+{
+	cout << "Preorder : ";
+	preorder(0);
+	cout << "\n";
+}
+
+void BinaryTree::preorder(int idx)
+{
+	// V L R
+	if (0 != values[idx])
+	{
+		cout << values[idx] << ",";
+		preorder(2 * idx + 1);
+		preorder(2 * idx + 2);
+	}
+}
+
+void BinaryTree::inorder()
+{
+	cout << "Inorder : ";
+	inorder(0);
+	cout << "\n";
+}
+
+void BinaryTree::inorder(int idx)
+{
+	// L V R
+	if (0 != values[idx])
+	{
+		inorder(2 * idx + 1);
+		cout << values[idx] << ",";
+		inorder(2 * idx + 2);
+	}
+}
+
+void BinaryTree::postorder()
+{
+	cout << "Postorder : ";
+	postorder(0);
+	cout << "\n";
+}
+
+void BinaryTree::postorder(int idx)
+{
+	// L R V
+	if (0 != values[idx])
+	{
+		postorder(2 * idx + 1);
+		postorder(2 * idx + 2);
+		cout << values[idx] << ",";
+	}
+}
+
+void BinaryTree::levelorder()
+{
+	cout << "Level order : ";
+
+	if (0 == values[0])
+	{
+		return;
+	}
+
+	int idx = 0;
+
+	using queueType = queue<pair<int, char>>;
+
+	queueType q;
+	q.push(queueType::value_type(0, values[idx]));
+
+	while (false == q.empty())
+	{
+		int idx = q.front().first;
+		int val = q.front().second;
+
+		q.pop();
+
+		cout << val << ",";
+
+		int left = 2 * idx + 1;
+		int right = 2 * idx + 2;
+
+		if (0 != values[left])
+			q.push(queueType::value_type(left, values[left]));
+
+		if (0 != values[right])
+			q.push(queueType::value_type(right, values[right]));
+	}
+
+	cout << "\n";
+}
+
+int BinaryTree::getCount()
+{
+	int count = getCount(0);
+	printf("Tree node count : %d\n", count);
+	return count;
+}
+
+int BinaryTree::getCount(int idx)
+{
+	if (0 != values[idx])
+	{
+		int left = 2 * idx + 1;
+		int right = 2 * idx + 2;
+
+		return 1 + getCount(left) + getCount(right);
+	}
+
+	return 0;
+}
+
+
+int BinaryTree::getHeight()
+{
+	int height = getHeight(0);
+	printf("Tree height : %d\n", height);
+	return height;
+}
+
+int BinaryTree::getHeight(int idx)
+{
+	if (0 != values[idx])
+	{
+		int left = getCount(2 * idx + 1);
+		int right = getCount(2 * idx + 2);
+
+		return 1 + (left < right ? right : left);
+	}
+
+	return 0;
+}
+
+void BinaryTree::insert_node(int idx, char value)
+{
+	values[idx] = value;
+}
+
+int main()
+{
+	BinaryTree* tree = new BinaryTree();
+
+	tree->insert_node(0, 'A');
+	tree->insert_node(1, 'B');
+	tree->insert_node(2, 'C');
+	tree->insert_node(3, 'D');
+	tree->insert_node(4, 'E');
+	tree->insert_node(5, 'F');
+
+	// 트리 순회(tree traversal)
+	tree->preorder();
+	tree->inorder();
+	tree->postorder();
+	tree->levelorder();
+
+	tree->getCount();
+	tree->getHeight();
+
+	delete tree;
+}
---
+++ 05.BinaryTree/BinaryTree(link)/main.cpp
@@ -0,0 +1,222 @@
+﻿#include <iostream>
+#include <queue>
+
+using namespace std;
+
+struct Node
+{
+	Node() : value(0), left(nullptr), right(nullptr) {}
+	Node(char value, Node* left, Node* right) : value(value), left(left), right(right) {}
+	char value;
+	Node* left;
+	Node* right;
+};
+
+class BinaryTree
+{
+public:
+	BinaryTree();
+	~BinaryTree();
+
+	void destroy_tree();
+
+	// tree traversal
+	void preorder();
+	void inorder();
+	void postorder();
+	void levelorder();
+
+	int getCount();
+	int getHeight();
+
+	void set_root(Node* node);
+
+private:
+	void destroy_tree(Node* leaf);
+
+	void preorder(Node* node);
+	void inorder(Node* node);
+	void postorder(Node* node);
+
+	int getCount(Node* node);
+	int getHeight(Node* node);
+
+	Node* root;
+};
+
+
+BinaryTree::BinaryTree() : root(nullptr)
+{
+}
+
+BinaryTree::~BinaryTree()
+{
+	destroy_tree();
+}
+
+void BinaryTree::destroy_tree()
+{
+	destroy_tree(root);
+}
+
+void BinaryTree::destroy_tree(Node* node)
+{
+	if (nullptr != node)
+	{
+		destroy_tree(node->left);
+		destroy_tree(node->right);
+		delete node;
+	}
+}
+
+void BinaryTree::preorder()
+{
+	cout << "Preorder : ";
+	preorder(root);
+	cout << "\n";
+}
+
+void BinaryTree::preorder(Node* node)
+{
+	// V L R
+	if (nullptr != node)
+	{
+		cout << node->value << ",";
+		preorder(node->left);
+		preorder(node->right);
+	}
+}
+
+void BinaryTree::inorder()
+{
+	cout << "Inorder : ";
+	inorder(root);
+	cout << "\n";
+}
+
+void BinaryTree::inorder(Node* node)
+{
+	// L V R
+	if (nullptr != node)
+	{
+		inorder(node->left);
+		cout << node->value << ",";
+		inorder(node->right);
+	}
+}
+
+void BinaryTree::postorder()
+{
+	cout << "Postorder : ";
+	postorder(root);
+	cout << "\n";
+}
+
+void BinaryTree::postorder(Node* node)
+{
+	// L R V
+	if (nullptr != node)
+	{
+		postorder(node->left);
+		postorder(node->right);
+		cout << node->value << ",";
+	}
+}
+
+void BinaryTree::levelorder()
+{
+	cout << "Level order : ";
+
+	if (nullptr == root)
+	{
+		return;
+	}
+
+	queue<Node*> q;
+	q.push(root);
+
+	while (false == q.empty())
+	{
+		Node* node = q.front();
+		q.pop();
+
+		cout << node->value << ",";
+
+		if (nullptr != node->left)
+			q.push(node->left);
+
+		if (nullptr != node->right)
+			q.push(node->right);
+	}
+
+	cout << "\n";
+}
+
+int BinaryTree::getCount()
+{
+	int count = getCount(root);
+	printf("Tree node count : %d\n", count);
+	return count;
+}
+
+int BinaryTree::getCount(Node* node)
+{
+	if (nullptr != node)
+	{
+		return 1 + getCount(node->left) + getCount(node->right);
+	}
+
+	return 0;
+}
+
+
+int BinaryTree::getHeight()
+{
+	int height = getHeight(root);
+	printf("Tree height : %d\n", height);
+	return height;
+}
+
+int BinaryTree::getHeight(Node* node)
+{
+	if (nullptr != node)
+	{
+		int left = getCount(node->left);
+		int right = getCount(node->right);
+
+		return 1 + (left < right ? right : left);
+	}
+
+	return 0;
+}
+
+
+void BinaryTree::set_root(Node* node)
+{
+	root = node;
+}
+
+int main()
+{
+	BinaryTree* tree = new BinaryTree();
+
+	Node* d = new Node('D', nullptr, nullptr);
+	Node* e = new Node('E', nullptr, nullptr);
+	Node* b = new Node('B', d, e);
+	Node* f = new Node('F', nullptr, nullptr);
+	Node* c = new Node('C', f, nullptr);
+	Node* a = new Node('A', b, c);
+
+	tree->set_root(a);
+
+	// 트리 순회(tree traversal)
+	tree->preorder();
+	tree->inorder();
+	tree->postorder();
+	tree->levelorder();
+
+	tree->getCount();
+	tree->getHeight();
+
+	delete tree;
+}
